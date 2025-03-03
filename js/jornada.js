15561b1f21bec70b0e8cc2d71ab9ca619101cfd2
YohanaManteca_ExamenGit <yohanamanteca@gmail.com>
2025-03-04 00:57:39 +0100

    cambios css header

--- js/jornada.js
+++ js/jornada.js
@@ -1,3 +1,8 @@
-document.getElementById('elegirCampamento').addEventListener('click', function() {
-    window.location.href = 'calendarioreserva.html';
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById('elegirCampamento1').addEventListener('click', function() {
+        window.location.href = 'calendarioreserva.html';
+    });
+    document.getElementById('elegirCampamento2').addEventListener('click', function() {
+        window.location.href = 'calendarioreserva.html';
+    });
 });
