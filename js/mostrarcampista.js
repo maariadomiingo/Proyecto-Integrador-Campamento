f21381cf939b2f7b4e597940465819435dfaac61
maariadomiingo <domingopuentemaria@gmail.com>
2025-03-03 13:39:56 +0100

    botones monitor y coordinador todos

--- js/mostrarcampista.js
+++ js/mostrarcampista.js
@@ -2,9 +2,15 @@ document.addEventListener("DOMContentLoaded", function () {
     // Obtener el ID de la URL
     const params = new URLSearchParams(window.location.search);
     const idCampista = params.get("id");
+    const identificacionMonitor = params.get('identificacion');
 
     if (!idCampista) {
         alert("No se proporcionó un ID de campista.");
+        return;
+    }
+
+    if (!identificacionMonitor) {
+        alert("No se proporcionó la identificación del monitor.");
         return;
     }
 
@@ -15,7 +21,8 @@ document.addEventListener("DOMContentLoaded", function () {
             "Content-Type": "application/x-www-form-urlencoded"
         },
         body: new URLSearchParams({
-            idCampista: idCampista 
+            idCampista: idCampista,
+            identificacionMonitor: identificacionMonitor
         })
     })
     .then(response => response.json())
@@ -35,4 +42,32 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("telefonoEmergencia").textContent = data.telefonoEmergencia;
     })
     .catch(error => console.error("Error al obtener datos del campista:", error));
+
+    // Funcionalidad para el botón "Atrás"
+    document.querySelector(".button").addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = `pasarlista.html?identificacion=${encodeURIComponent(identificacionMonitor)}`;
+    });
+
+    const botonSalir = document.querySelector('.circulo-salir');
+
+    // Funcionalidad del botón de salir
+    if (botonSalir) {
+        botonSalir.addEventListener('click', function () {
+            window.location.href = '../html/login.html';
+        });
+    } else {
+        console.error("El botón 'Salir' no fue encontrado en el DOM.");
+    }
+
+    const perfilMonitor = document.querySelector('.circulo');
+    if (perfilMonitor) {
+        perfilMonitor.addEventListener('click', function (event) {
+            event.preventDefault();
+            console.log("Redirigiendo a perfil monitor con identificacion:", identificacionMonitor);
+            window.location.href = `../html/perfilMonitor.html?identificacion=${encodeURIComponent(identificacionMonitor)}`;
+        });
+    } else {
+        console.error("El botón 'Perfil' no fue encontrado en el DOM.");
+    }
 });
